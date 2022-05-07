d25f317bf9c8e30c65ccee9dbe9d3b423498193a
Noe Sola <ariadnnasc@gmail.com>
2022-05-07 11:54:34 +0200

    feat: component Screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Wrapper from "./components/Wrapper"
+import Screen from "./components/Screen"
 
 const App = () =>{
     return (
         <div className="App">
             <Wrapper>
-                Hola mundo
+                <Screen value="0"/>
+            
             </Wrapper>
         </div>
     )
---
+++ src/components/Screen.jsx
@@ -0,0 +1,10 @@
+import "./Screen.css";
+
+const Screen = ({value}) =>{
+    return (
+        <div className="screen">
+            {value}
+        </div>
+    )
+}
+export default Screen;
